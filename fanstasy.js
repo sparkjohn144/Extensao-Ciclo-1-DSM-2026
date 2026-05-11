19a3c223860d9cf48b5bff30235d5c1dbcf6a724
Eron Padilla <erongpoliveira@gmail.com>
2026-05-11 14:32:05 -0300

    validação melhorada

--- fanstasy.js
+++ fanstasy.js
@@ -3,16 +3,31 @@ function mudarPagina()
     event.preventDefault(); //essa porra aqui impede que o forms seja mandado direto
 
     let input = document.getElementById('senha-barra');
+    let input2 = document.getElementById('email-barra');
     let valor = input.value.trim();
+    let valor2 = input.value.trim();
     
-    if (valor !== "")
+    if (valor !== "" && valor2 !== "")
     {
         window.location.href = 'index.html';
         alert('Login realizado com sucesso! Seja Bem-Vindo')
     }
     else
     {
-        alert('Preencha os Campos Necessários ');   
+        alert('Preencha os Campos Necessários ');
+        if(valor == "" && valor2 == "")
+        {
+            input2.focus();
+        }
+        else if (valor2 == "")
+        {
+            input2.focus();
+        }
+        else if(valor == "")
+        {
+            input.focus();
+        }
     }
 
 }
+
