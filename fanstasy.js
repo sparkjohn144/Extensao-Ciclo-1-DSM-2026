02ae2ee606b061e7c1cd83261d4a5d748383ec83
Eron Padilla <erongpoliveira@gmail.com>
2026-05-11 00:28:04 -0300

    js pra validar uns bgl ai

---
+++ fanstasy.js
@@ -0,0 +1,21 @@
+function mudarPagina()
+{
+    event.preventDefault(); //essa porra aqui impede que o forms seja mandado direto
+
+    let input = document.getElementById('senha-barra');
+    let valor = input.value.trim();
+    
+    if (valor !== "")
+    {
+        window.location.href = 'index.html';
+        alert('Login realizado com sucesso! Seja Bem-Vindo')
+    }
+    else
+    {
+        alert('Preencha os Campos Necessários ');   
+    }
+
+
+
+    //window.location.href = 'index.html';
+}
